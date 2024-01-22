3c1eef7bdd3515ff03cc9df834f91bf106a6572c
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-01-22 11:54:29 +0000

    Refactored createTabEvents into a module

---
+++ modules/tab-events.js
@@ -0,0 +1,51 @@
+(function () {
+
+    'use strict';
+
+    module.exports = exports = createTabEvents;
+
+    /**
+     * Creates the tab event listeners. These are used to switch the currently
+     * active tab when the user clicks on a tab.
+     */
+    function createTabEvents () {
+        let rawTabs = [...document.getElementsByClassName('tab')]
+            .slice(1);
+
+        /**
+         * Returns the content element that corresponds to the passed navbar tab
+         * @param {HTMLElement} tab - The navbar tab element
+         * @returns {HTMLElement} - The corresponding content element
+         */
+        function getTabContent (tab) {
+            return document.getElementById(tab.id + "-content");
+        }
+
+        /**
+         * Makes all tabs inactive
+         */
+        function makeAllTabsInactive () {
+            rawTabs.forEach((tab) => {
+                tab.classList.remove('active');
+                getTabContent(tab).classList.remove('active');
+            });
+        }
+
+        /**
+         * Makes the passed tab active
+         * @param {HTMLElement} tab - The tab to make active
+         */
+        function makeTabActive (tab) {
+            tab.classList.add('active');
+            getTabContent(tab).classList.add('active');
+        }
+
+        rawTabs.forEach((tab) => {
+            tab.addEventListener('click', () => {
+                makeAllTabsInactive();
+                makeTabActive(tab);
+            });
+        });
+    }
+
+})();
--- osc.js
+++ osc.js
@@ -2,46 +2,6 @@
 
     'use strict';
 
-    function createTabEvents () {
-        let rawTabs = [...document.getElementsByClassName('tab')]
-            .slice(1);
-
-        /**
-         * Returns the content element that corresponds to the passed navbar tab
-         * @param {HTMLElement} tab - The navbar tab element
-         * @returns {HTMLElement} - The corresponding content element
-         */
-        function getTabContent (tab) {
-            return document.getElementById(tab.id + "-content");
-        }
-
-        /**
-         * Makes all tabs inactive
-         */
-        function makeAllTabsInactive () {
-            rawTabs.forEach((tab) => {
-                tab.classList.remove('active');
-                getTabContent(tab).classList.remove('active');
-            });
-        }
-
-        /**
-         * Makes the passed tab active
-         * @param {HTMLElement} tab - The tab to make active
-         */
-        function makeTabActive (tab) {
-            tab.classList.add('active');
-            getTabContent(tab).classList.add('active');
-        }
-
-        rawTabs.forEach((tab) => {
-            tab.addEventListener('click', () => {
-                makeAllTabsInactive();
-                makeTabActive(tab);
-            });
-        });
-    }
-
-    createTabEvents();
+    require('./modules/tab-events')();
 
 })();
