623648d9d7a59c61385ad16f2a1d726c5012d4f7
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-01-27 10:33:59 +0000

    Added demo tabs content

--- osc.js
+++ osc.js
@@ -4,7 +4,11 @@
 
     const tabEventsController = require('./modules/tab-events-controller');
 
-    tabEventsController.createTabEvents(document.getElementById('navbar'),
-        [0], true);
+    tabEventsController.createTabEvents(
+        document.getElementById('navbar'), [0], true);
+    tabEventsController.createTabEvents(
+        document.getElementById('settings-tab-content'));
+    tabEventsController.createTabEvents(
+        document.getElementById('library-tab-content'));
 
 })();
