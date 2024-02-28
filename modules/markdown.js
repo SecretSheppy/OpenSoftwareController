8a0d5204c9ee6fa3fe546b0646aaaccf6d87e9cb
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-02-28 23:20:31 +0000

    feat(parser): added input sanitization for improved security
    The initially published version of this parser didn't account for any pre inserted html elements that could allow for injection of malicious styles/scripts into the application, making the parser a large security risk.
    
    This has now been addressed as dangerous elements will now be identified and removed before the markdown input is parsed.

--- modules/markdown.js
+++ modules/markdown.js
@@ -13,6 +13,136 @@
 
     const RULES = [
 
+        // Sanitization
+
+        // Script detection
+        [
+            /<script(.*)>(.*)<\/script>/g,
+            "<div>WARNING: Script detected and removed</div>"
+        ],
+
+        // iframe detection
+        [
+            /<iframe(.*)>(.*)<\/iframe>/g,
+            "<div>WARNING: Iframe detected and removed</div>"
+        ],
+
+        // object detection
+        [
+            /<object(.*)>(.*)<\/object>/g,
+            "<div>WARNING: Object detected and removed</div>"
+        ],
+
+        // embed detection
+        [
+            /<embed(.*)>/g,
+            "<div>WARNING: Embed detected and removed</div>"
+        ],
+
+        // link detection
+        [
+            /<link(.*)>/g,
+            "<div>WARNING: Link detected and removed</div>"
+        ],
+
+        // style detection
+        [
+            /<style(.*)>(.*)<\/style>/g,
+            "<div>WARNING: Style detected and removed</div>"
+        ],
+
+        // img detection
+        [
+            /<img(.*)>/g,
+            "<div>WARNING: Image detected and removed</div>"
+        ],
+
+        // svg detection
+        [
+            /<svg(.*)>(.*)<\/svg>/g,
+            "<div>WARNING: SVG detected and removed</div>"
+        ],
+
+        // form detection
+        [
+            /<form(.*)>(.*)<\/form>/g,
+            "<div>WARNING: Form detected and removed</div>"
+        ],
+
+        // input detection
+        [
+            /<input(.*)>/g,
+            "<div>WARNING: Input detected and removed</div>"
+        ],
+
+        // button detection
+        [
+            /<button(.*)>(.*)<\/button>/g,
+            "<div>WARNING: Button detected and removed</div>"
+        ],
+
+        // select detection
+        [
+            /<select(.*)>(.*)<\/select>/g,
+            "<div>WARNING: Select detected and removed</div>"
+        ],
+
+        // textarea detection
+        [
+            /<textarea(.*)>(.*)<\/textarea>/g,
+            "<div>WARNING: Textarea detected and removed</div>"
+        ],
+
+        // meta detection
+        [
+            /<meta(.*)>/g,
+            "<div>WARNING: Meta detected and removed</div>"
+        ],
+
+        // title detection
+        [
+            /<title(.*)>(.*)<\/title>/g,
+            "<div>WARNING: Title detected and removed</div>"
+        ],
+
+        // head detection
+        [
+            /<head(.*)>(.*)<\/head>/g,
+            "<div>WARNING: Head detected and removed</div>"
+        ],
+
+        // body detection
+        [
+            /<body(.*)>(.*)<\/body>/g,
+            "<div>WARNING: Body detected and removed</div>"
+        ],
+
+        // html detection
+        [
+            /<html(.*)>(.*)<\/html>/g,
+            "<div>WARNING: HTML detected and removed</div>"
+        ],
+
+        // DOCTYPE detection
+        [
+            /<!DOCTYPE(.*)>/g,
+            "<div>WARNING: DOCTYPE detected and removed</div>"
+        ],
+
+        // audio detection
+        [
+            /<audio(.*)>(.*)<\/audio>/g,
+            "<div>WARNING: Audio detected and removed</div>"
+        ],
+
+        // video detection
+        [
+            /<video(.*)>(.*)<\/video>/g,
+            "<div>WARNING: Video detected and removed</div>"
+        ],
+
+        // Markdown
+
         // Headers
         [/#{6}\s?(.+)/g, "<h6>$1</h6>"],
         [/#{5}\s?(.+)/g, "<h5>$1</h5>"],
